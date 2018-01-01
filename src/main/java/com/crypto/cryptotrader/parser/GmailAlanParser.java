2d3d391707112fe2d826f0e8c8848cd598c81708
psuchecki <piotr.suchecki.ext@gmail.com>
2018-01-01 20:52:01 +0100

    Add alan parser when no currency code is included

--- src/main/java/com/crypto/cryptotrader/parser/GmailAlanParser.java
+++ src/main/java/com/crypto/cryptotrader/parser/GmailAlanParser.java
@@ -14,14 +14,18 @@ import com.crypto.cryptotrader.shortorder.ShortOrderExecutor;
 public class GmailAlanParser {
 
 	public static final String ALAN_REGEX_PATTERN = "(.*alanmasters you follow published new idea)(.*)(\\(\\w+\\)).*";
+	public static final String WITHOUT_CODE_PATTERN = "(.*alanmasters you follow published new idea )(\\w+).*";
+
+	public static final Pattern ALAN_PATTERN = Pattern.compile(ALAN_REGEX_PATTERN);
+	public static final Pattern ALAN_PATTERN_WITHOUT_CODE = Pattern.compile(WITHOUT_CODE_PATTERN);
+
 	public static final String REMOVE_PARENTHESIS_REGEX = "[()]";
 	@Autowired
 	private ShortOrderExecutor shortOrderExecutor;
 
 	public void parseMessage(String message) throws IOException {
-		Pattern pattern = Pattern.compile(ALAN_REGEX_PATTERN);
-		Matcher matcher = pattern.matcher(message);
-		if (matcher.matches()) {
+		Matcher matcher = ALAN_PATTERN.matcher(message);
+		if (matcher.matches() || (matcher = ALAN_PATTERN_WITHOUT_CODE.matcher(message)).matches()) {
 			String baseCurrencyCode = matcher.group(matcher.groupCount());
 			if (StringUtils.isNotBlank(baseCurrencyCode)) {
 				baseCurrencyCode = baseCurrencyCode.replaceAll(REMOVE_PARENTHESIS_REGEX, StringUtils.EMPTY);
