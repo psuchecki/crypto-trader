876b5cb2a3969019e634e9050d24d4309187454d
psuchecki <piotr.suchecki.ext@gmail.com>
2018-01-14 17:10:44 +0800

    Add logging when no currency code is recognized.

--- src/main/java/com/crypto/cryptotrader/parser/TwitterParser.java
+++ src/main/java/com/crypto/cryptotrader/parser/TwitterParser.java
@@ -6,6 +6,8 @@ import twitter4j.Status;
 import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,7 @@ import com.crypto.cryptotrader.shortorder.ShortOrderExecutor;
 
 @Component
 public class TwitterParser {
+	private static final Logger logger = LoggerFactory.getLogger(TwitterParser.class);
 	public static final String COIN_OF_THE_WEEK_PATTERN = "COIN OF THE WEEK";
 
 	@Autowired
@@ -28,8 +31,11 @@ public class TwitterParser {
 		if (StringUtils.isNotEmpty(occurence) && status.getMediaEntities().length != 0) {
 			String imageUrl = status.getMediaEntities()[0].getMediaURL();
 			String currencyCode = imageParser.getCurrencyCodeFromImage(imageUrl);
-			shortOrderExecutor.executeShortBidOrder(currencyCode);
+			if (StringUtils.isNotEmpty(currencyCode)) {
+				shortOrderExecutor.executeShortBidOrder(currencyCode);
+			} else {
+				logger.warn("TWITTER: Cannot recognize currency code in image {}" + imageUrl);
+			}
 		}
-
 	}
 }
